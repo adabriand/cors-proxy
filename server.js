e9e7cdcb21e8414b721c5d62b8f491c1ea918edd
Adabriand Furtado <adabriand@gmail.com>
2020-08-07 00:02:52 -0300

    Update server.js

--- server.js
+++ server.js
@@ -56,8 +56,12 @@ app.all('*', function (req, res, next) {
             headers: headers,
         };
         
-        if (targetProxyHost) options['host'] = targetProxyHost;
-        if (targetProxyPort) options['proxy'] = 'https://' + targetProxyUsername + ':' + targetProxyPassword + '@' + targetProxyHost + ':' + targetProxyPort;
+//         if (targetProxyHost) options['host'] = targetProxyHost;
+        if (targetProxyPort) {
+            request.defaults({proxy: 'https://' + targetProxyUsername + ':' + targetProxyPassword + '@' + targetProxyHost + ':' + targetProxyPort});
+        }
+            
+//             options['proxy'] = 'https://' + targetProxyUsername + ':' + targetProxyPassword + '@' + targetProxyHost + ':' + targetProxyPort;
         
         console.log(options);
         console.log(headers);
