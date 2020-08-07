dc2f5e65784d26248ec9ef9fb9f13fed6de00237
Adabriand Furtado <adabriand@gmail.com>
2020-08-07 01:48:13 -0300

    Update server.js

--- server.js
+++ server.js
@@ -52,10 +52,10 @@ app.all('*', function (req, res, next) {
         var options = { 
             url: targetURL + req.url, 
             method: req.method, 
-            body: Buffer.from(req.body, 'base64').toString( 'binary'),
+            body: Buffer.from(req.body, 'base64').toString('binary'),
             headers: headers,
             json: false,
-            encoding: null,
+            encoding: 'binary',
         };
         
             
