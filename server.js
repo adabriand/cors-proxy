26655963c2d7f2796eb442f8c23f2aa7f528149f
Adabriand Furtado <adabriand@gmail.com>
2020-08-07 00:10:37 -0300

    Update server.js

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.all('*', function (req, res, next) {
         var options = { 
             url: targetURL + req.url, 
             method: req.method, 
-            body: req.body,
+            body: req.body.toString('binary'),
             headers: headers,
         };
         
